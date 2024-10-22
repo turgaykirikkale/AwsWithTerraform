d39afa3dd7773ad0564a517bc15bbd9a9b63f24f
turgaykirikkale <63849042+turgaykirikkale@users.noreply.github.com>
2024-10-22 18:35:45 +0300

    Delete Services/EC2 directory

--- Services/EC2/modules/ec2/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "instance" {
-  ami           = lookup(var.ami_map, var.ami_name)  # AMI adı ile AMI ID'sini bul
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name  # Her instance'a isim atanır
-  }
-}
--- Services/EC2/modules/ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_IP" {
-  value = aws_instance.instance.public_ip
-}
